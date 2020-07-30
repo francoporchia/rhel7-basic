c97fada6d8315f03453b8a753fe4f384ec75a2c9
francoporchia <64796193+francoporchia@users.noreply.github.com>
2020-07-30 16:29:50 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,4 +2,5 @@
 #!/bin/bash
 yum update -y
 yum upgrade -y
-yum install -y epel-release vim htop net-tools moreutils htop
+yum install -y epel-release 
+yum install -y vim htop net-tools moreutils htop
