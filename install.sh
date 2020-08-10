d45ce7d847153ecec7e0a67dc3f95bd36ebaf32f
francoporchia <64796193+francoporchia@users.noreply.github.com>
2020-08-10 11:02:07 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,5 +3,10 @@
 yum update -y
 yum upgrade -y
 yum install -y epel-release 
-yum install -y vim htop net-tools moreutils htop wget curl ntp
+yum install -y vim htop net-tools moreutils htop wget curl ntp mlocate
 yum install -y yum-config-manager yum-utils
+updatedb
+timedatectl set-timezone America/Argentina/Buenos_Aires
+echo "#######################################"
+echo "Ajustar servidor de ntp en /etc/ntpd.conf"
+echo "#######################################"
